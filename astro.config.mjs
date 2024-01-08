4a17c489505a9ff52c9a7883cacdf7d8c05dec92
username <90975164+adamswebsite@users.noreply.github.com>
2024-01-08 12:26:48 +0100

    yes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://wordpress-castellon.github.io',
+    site: 'https://uxcelona.github.io',
 });
